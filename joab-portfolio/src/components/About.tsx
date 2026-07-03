6a7db7ccd87b134250eaf4dad156ae98efccb7b2
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-07-03 17:43:51 +0100

    feat: added download cv button

--- joab-portfolio/src/components/About.tsx
+++ joab-portfolio/src/components/About.tsx
@@ -1,3 +1,6 @@
+import { ArrowDownIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
 export const About = () => {
     const imgClass = "mx-auto md:mx-0 size-56 md:size-64 lg:size-72 rounded-full object-cover shrink-0 border-4 border-background shadow-xl";
     return (
@@ -15,6 +18,9 @@ export const About = () => {
                     <p className="text-muted-foreground text-base md:text-lg leading-relaxed max-w-prose">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
+                    <Button variant="outline" size="lg">
+                        Download CV <ArrowDownIcon className="size-4" />
+                    </Button>
                 </div>
             </div>
         </section>
