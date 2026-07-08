46680f902c55c3795d3f2a1a31e0259219d23310
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-07-08 13:17:00 +0100

    feat: added pen pic download button

--- joab-portfolio/src/components/About.tsx
+++ joab-portfolio/src/components/About.tsx
@@ -18,9 +18,14 @@ export const About = () => {
                     <p className="text-muted-foreground text-base md:text-lg leading-relaxed max-w-prose">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
-                    <Button variant="outline" size="lg">
-                        Download CV <ArrowDownIcon className="size-4" />
-                    </Button>
+                    <div className="flex flex-row gap-2">
+                        <Button variant="outline" size="lg">
+                            Download CV <ArrowDownIcon className="size-4" />
+                        </Button>
+                        <Button variant="outline" size="lg">
+                            Download Pen Pic <ArrowDownIcon className="size-4" />
+                        </Button>
+                    </div>
                 </div>
             </div>
         </section>
