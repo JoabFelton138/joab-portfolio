c5b935f3d733df2c38d94b397c6e83ff82beb7fd
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-07-08 12:56:10 +0100

    feat: added react icons, refactored classes into variables to prevent repetition

--- joab-portfolio/src/components/Footer.tsx
+++ joab-portfolio/src/components/Footer.tsx
@@ -1,60 +1,83 @@
-import { Cloud, CodeIcon, Layers } from "lucide-react";
+import { Cloud, Layers } from "lucide-react";
 import { IconCircle } from "./IconCircle";
+import { SiReact, SiShadcnui, SiTailwindcss, SiTypescript, SiVite } from "react-icons/si";
+import { FaAws } from "react-icons/fa";
 
 export const Footer = () => {
 
-    const codeIcon = <CodeIcon className="size-6 text-muted-foreground" />;
+    const iconClass = "size-4 text-muted-foreground";
+    const iconCircleClass = "size-12 shrink-0";
+    const titleContainerClass = "flex items-center gap-3";
+    const titleIconClass = "size-6 text-muted-foreground";
+    const badgeClass = "inline-flexitems-center gap-2 rounded-lg border border-border bg-background px-3 py-2";
+    const spanClass = "text-sm text-muted-foreground";
 
     const techStack = [
         {
             name: "React",
-            icon: codeIcon
+            icon: <SiReact className={iconClass} />
         },
         {
             name: "Vite",
-            icon: codeIcon
+            icon: <SiVite className={iconClass} />
         },
         {
             name: "TypeScript",
-            icon: codeIcon
+            icon: <SiTypescript className={iconClass} />
         },
         {
             name: "Tailwind CSS",
-            icon: codeIcon
+            icon: <SiTailwindcss className={iconClass} />
         },
         {
             name: "Shadcn UI",
-            icon: codeIcon
+            icon: <SiShadcnui className={iconClass} />
         }
     ];
 
     const hostedBy = {
         name: "Amazon Web Services",
-        icon: <Cloud/>
+        icon: <FaAws className="size-6 text-muted-foreground" />
     };
 
     return (
-        <footer className="bg-background border-t border-borderp-8">
-            <div className="mx-auto grid max-w-7xl grid-cols-1 md:grid-cols-[1.5fr_1fr] gap-8 p-8 md:gap-6">
-                <div className="space-y-4 border-b border-border pb-8 md:border-b-0 md:border-r md:pb-0 md:pr-4">
-                    <div className="flex items-center gap-3">
-                          <IconCircle className="size-12 shrink-0">
-                                <Layers className="size-6 text-muted-foreground" />
+        <footer className="border-t border-border">
+        <div className="mx-auto w-full max-w-7xl px-8 py-8">
+            <div className="grid grid-cols-1 md:grid-cols-[1.3fr_1fr] gap-y-6 gap-x-4 md:gap-6">
+                <div className="space-y-4 border-b border-border pb-8 md:border-b-0 md:border-r md:pb-0 md:pr-6">
+                    <div className={titleContainerClass}>
+                          <IconCircle className={iconCircleClass}>
+                                <Layers className={titleIconClass} />
                             </IconCircle>
                         <h3 className="font-semibold">Built with</h3>
                     </div>
                     <div className="flex flex-wrap gap-2">
                         {techStack.map((tech) => (
                             <div 
-                                className="flex items-center gap-4 rounded-lg border border-border bg-background px-3 py-2" 
+                                className={`flex ${badgeClass}`} 
                                 key={tech.name}
                             >
                                 {tech.icon}
-                                <span className="text-sm text-muted-foreground">{tech.name}</span>
+                                <span className={spanClass}>{tech.name}</span>
                             </div>
                         ))}
                     </div>
                 </div>
+                <div className="space-y-4">
+                    <div className={titleContainerClass}>
+                          <IconCircle className={iconCircleClass}>
+                                <Cloud className={titleIconClass} />
+                            </IconCircle>
+                        <h3 className="font-semibold">Hosted On</h3>
+                    </div>
+                    <div className={`inline-flex ${badgeClass}`}>
+                        {hostedBy.icon}
+                        <span className={spanClass}>
+                            {hostedBy.name}
+                        </span>
+                    </div>
+                </div>
+            </div>
             </div>
         </footer>
     );
