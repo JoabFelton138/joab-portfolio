7447c55921e931208af68ad1c9922aa1dc98eebe
JoabFelton138 <joab_felton91@hotmail.co.uk>
2026-07-14 16:20:20 +0100

    style: chacnged cv button to primary

--- joab-portfolio/src/components/About.tsx
+++ joab-portfolio/src/components/About.tsx
@@ -4,14 +4,14 @@ import { Spinner } from "./ui/spinner";
 import { useDownloadDoc } from "@/lib/useDownloadDoc";
 
 export const About = () => {
-
- 
-
     const cv = useDownloadDoc("cv");
     const penPic = useDownloadDoc("pen-pic");
-    const aboutMeText = `An inquisitive Developer with a First-Class Computer Science degree, specialising in building modern, user-focused applications that translate UI/UX designs into intuitive, production-ready interfaces. Proficient in React, Next.js, TypeScript, and modern JavaScript tooling, with strengths in writing clean, maintainable code, implementing responsive and accessible designs, and applying engineering best practices to ensure reliable delivery. Experienced working within cross-functional, client-facing teams, integrating APIs, and balancing technical constraints with stakeholder needs to deliver meaningful business outcomes.`;
-    const imgClass = "size-45 md:size-55 lg:size-65 rounded-full object-cover shrink-0 border-4 border-background shadow-xl";
-    
+
+    const aboutMeText =
+        "An inquisitive Developer with a First-Class Computer Science degree, specialising in building modern, user-focused applications that translate UI/UX designs into intuitive, production-ready interfaces. Proficient in React, Next.js, TypeScript, and modern JavaScript tooling, with strengths in writing clean, maintainable code, implementing responsive and accessible designs, and applying engineering best practices to ensure reliable delivery. Experienced working within cross-functional, client-facing teams, integrating APIs, and balancing technical constraints with stakeholder needs to deliver meaningful business outcomes.";
+    const imgClass =
+        "size-45 md:size-55 lg:size-65 rounded-full object-cover shrink-0 border-4 border-background shadow-xl";
+
     return (
         <section className="mx-auto w-full max-w-7xl px-8 pt-12 pb-6" id="about">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-6 gap-y-6 items-center">
@@ -23,19 +23,30 @@ export const About = () => {
                     />
                 </div>
                 <div className="lg:col-span-2 space-y-3 text-center lg:text-left lg:pr-6">
-                    <h1 className="text-2xl md:text-3xl font-semibold tracking-tight">
-                        Hi, I'm Joab
-                    </h1>
+                    <div className="space-y-1">
+                        <h1 className="text-2xl md:text-3xl font-semibold tracking-tight">
+                            Hi, I'm Joab
+                        </h1>
+                    </div>
                     <p className="text-muted-foreground text-sm md:text-base leading-relaxed max-w-prose mx-auto lg:mx-0">
                         {aboutMeText}
                     </p>
                     <div className="flex flex-row flex-wrap justify-center lg:justify-start gap-2">
-                        <Button variant="outline" size="lg" onClick={() => cv.handleClickDownload()} className="cursor-pointer">
-                            Download CV 
+                        <Button
+                            size="lg"
+                            onClick={() => cv.handleClickDownload()}
+                            className="cursor-pointer"
+                        >
+                            Download CV
                             {cv.isDownloading ? <Spinner /> : <ArrowDownIcon className="size-4" />}
                         </Button>
-                        <Button variant="outline" onClick={() => penPic.handleClickDownload()} size="lg" className="cursor-pointer">
-                            Download Pen Pic 
+                        <Button
+                            variant="outline"
+                            size="lg"
+                            onClick={() => penPic.handleClickDownload()}
+                            className="cursor-pointer"
+                        >
+                            Download Pen Pic
                             {penPic.isDownloading ? <Spinner /> : <ArrowDownIcon className="size-4" />}
                         </Button>
                     </div>
@@ -43,4 +54,4 @@ export const About = () => {
             </div>
         </section>
     );
-}
+};
